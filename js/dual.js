68a4e1ad972fe0f373b3fd9c7e009ccd1e50d79b
Hilton Perantunes <hilton.perantunes@voxco.com>
2023-04-16 10:27:15 -0400

    Prevent globe size zero crash

--- js/dual.js
+++ js/dual.js
@@ -23,16 +23,16 @@ var dual = (function () {
 
         for (let centre in facesAroundPoint) {
             let neighbours = facesAroundPoint[centre];
-        
+
             // sets face's "type" as required by d3.geodesic
             // sets simple incremental id for each face
             var dual = {
                 "type": "Polygon",
                 "id": id++
             };
-        
+
             var orderedFaces = [neighbours[0]];
-        
+
             function addValidCandidate(candidate) {
                 if (neighbours.includes(candidate) && !orderedFaces.includes(candidate)) {
                     orderedFaces.push(candidate);
@@ -40,32 +40,32 @@ var dual = (function () {
                 }
                 return false;
             }
-        
+
             while (orderedFaces.length < neighbours.length) {
                 var points = orderedFaces[orderedFaces.length - 1].coordinates[0].map(normalisePoint);
                 facesAroundPoint[points[points.lastIndexOf(centre) - 1]].some(addValidCandidate);
             }
-        
+
             orderedFaces.push(orderedFaces[0]);
-        
+
             // sets face's coordinates
             dual.coordinates = [orderedFaces.map(d3.geoCentroid)];
-        
+
             // adds face's centroid coordinates
             dual.c = d3.polygonCentroid(dual.coordinates[0]);
             dual.c[0] = parseFloat(dual.c[0].toFixed(3));
             dual.c[1] = parseFloat(dual.c[1].toFixed(3));
-        
+
             // rounds each coordinate to 3 decimal places
             dual.coordinates[0].map(function (face) {
                 face[0] = parseFloat(face[0].toFixed(3));
                 face[1] = parseFloat(face[1].toFixed(3));
             });
-        
+
             for (var f = 0; f < neighbours.length; ++f) {
                 neighbours[f].duals.push(dual);
             }
-        
+
             duals.push(dual);
         }
 
